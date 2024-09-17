ec0f4db785c27834f2fe46415f92b33437555cba
mka <mka@A34PC14>
2024-09-17 10:02:38 +0300

    всьо зробила

--- 02-robat/Program.cs
+++ 02-robat/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("robat!");
+string nickname = Console.ReadLine();
+int C = int.Parse(Console.ReadLine());
+Console.WriteLine($"You are {(C * 9 / 5) + 32}");
+
+
+
+
